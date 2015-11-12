cb813bf91073b6fbedea59db9a39b87006be2095
Jeff <doongka@gmail.com>
2015-11-12 15:24:09 +1000

    Modified to obtain start and goal points in terms of image points

--- Path/iterative_sol_ex.m
+++ Path/iterative_sol_ex.m
@@ -1,16 +1,26 @@
+
+%Load localisation parameters
 load('local.mat');
-desired = [0 0];
+
+%Set desired start point
+desired_start = [0 0];
+
+%Set desired end point
+desired_end = [100 100];
+
 epsilon = 1.0;
 found = 0;
+
+%start clock
 tic
 for i = 1:320
     if found == 1
         break
     end
     for j = 1:240
-        pw = pointsToWorld(cameraParams, R, t, [i j]);
-        if abs(pw(1)-desired(1)) < epsilon
-            if abs(pw(2)-desired(2)) < epsilon
+        world_start = pointsToWorld(cameraParams, R, t, [i j]);
+        if abs(world_start(1)-desired_start(1)) < epsilon
+            if abs(world_start(2)-desired_start(2)) < epsilon
                 found = 1;
                 break;
             end
@@ -18,3 +28,27 @@ for i = 1:320
     end
 end
 toc
+
+%reset conditions
+epsilon = 1.0;
+found = 0;
+
+%restart clock
+tic
+for i = 1:320
+    if found == 1
+        break
+    end
+    for j = 1:240
+        world_end = pointsToWorld(cameraParams, R, t, [i j]);
+        if abs(world_end(1)-desired_end(1)) < epsilon
+            if abs(world_end(2)-desired_end(2)) < epsilon
+                found = 1;
+                break;
+            end
+        end
+    end
+end
+toc
+world_start
+world_end
