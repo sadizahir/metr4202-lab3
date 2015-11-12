d994e355e336bf17d6885fed6e54e4e815092675
Jeff <doongka@gmail.com>
2015-11-12 20:34:29 +1000

    sdfadsfs

---
+++ task1.m
@@ -0,0 +1,15 @@
+worldPoints_path
+l = size(worldPoints_path,1);
+z = 46
+coords = zeros(l, 3);
+
+for i = 1:l
+    coords(i,1) = worldPoints_path(i,1)/10;
+    coords(i,2) = worldPoints_path(i,2)/10;
+    coords(i,3) = z;
+end
+coords
+ 
+ for i = 1:l-1
+     find_path(coords(i,:), coords(i+1,:))
+ end
